df8621266bc44ceedbd1ebadfe5ba006bb17a926
jv <jv@forwardslash-development.io>
2020-10-20 14:12:40 -0500

    added style and format

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -20,13 +20,13 @@ export default ({ data }) => {
         <div className="container text-center">
           <i className="fas fa-hard-hat text-7xl text-secondary-blue pt-4 pb-2"></i>
           <h1 className="text-2xl font-semibold">Blog under construction</h1>
-          <p className="mt-3">
+          <p className="mt-3 pb-4">
             Welcome! Pardon our dust while we develop this feature. The Forwardslash Development blog where we write, when
             we write about progressive web application development. The addition of a blog feature has been put to a task
             card and is waiting to be moved to the "in progress" column. Stay tuned!
         </p>
         </div>
-        <IndexWrapper>
+        <IndexWrapper className="container text-center" >
           {data.allMdx.nodes.map(({ id, fields, excerpt, frontmatter }) => (
             <PostWrapper key={id}>
               <Link to={fields.slug}>
@@ -55,7 +55,7 @@ export const query = graphql`
         excerpt(pruneLength: 250)
         frontmatter {
           title
-          date
+          date(formatString: "YYYY MMMM Do")
         }
         fields {
           slug
